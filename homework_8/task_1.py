a38dfccc97f8718b14770f0fce49e4e24b4519f1
ViktoriiaYA-TRUTH <123602119+ViktoriiaYA-TRUTH@users.noreply.github.com>
2023-03-04 18:22:16 +0200

    homework_8 task_1

--- homework_8/task_1.py
+++ homework_8/task_1.py
@@ -1,21 +1,11 @@
 def index(lst, elem=None):
     for idx, item in enumerate(lst):
-        if isinstance(item, int):
-            item = str(item)
-            if item == elem:
-                return idx
-        elif isinstance(item, str):
-            if item == elem:
-                return idx
-        elif isinstance(item, tuple):
-            elem = tuple(elem)
+        if item == elem:
             return idx
-        else:
-            return None
 
 
 def main():
-    lst = [1, 2, 555, 'b', (4, 7)]
+    lst = ['1', 2, 555, 'b', (4, 7)]
     elem = input('Enter an element:')
     print(index(lst, elem))
 
