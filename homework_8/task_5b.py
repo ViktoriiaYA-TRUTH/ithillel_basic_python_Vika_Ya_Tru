d620d5810dcca8b9340a762a44f3896486cb6aeb
ViktoriiaYA-TRUTH <123602119+ViktoriiaYA-TRUTH@users.noreply.github.com>
2023-03-06 00:04:36 +0200

    homework_8 task_5b

--- homework_8/task_5b.py
+++ homework_8/task_5b.py
@@ -1,11 +1,9 @@
 def get_max_digit(number):  # returns int
     max_digit = 0
-    for i in range(12):
-        if number % 10 > max_digit:
-            max_digit = number % 10
-            number = number//10
-        else:
-            number = number//10
+    while number:
+        a = number % 10
+        max_digit = max(a, max_digit)
+        number = number // 10
     return max_digit
 
 
