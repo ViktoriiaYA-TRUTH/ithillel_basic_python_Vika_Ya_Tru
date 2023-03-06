390cb3acc1be6e5c7da5594423287642b9c3d9ab
ViktoriiaYA-TRUTH <123602119+ViktoriiaYA-TRUTH@users.noreply.github.com>
2023-03-06 18:46:32 +0200

    task_2

--- homework_9/task_2.py
+++ homework_9/task_2.py
@@ -1,14 +1,11 @@
 import random
 
+
 def diff_odd_even(num_limit, lower_bound, upper_bound): # returns int
     number = [random.randint(lower_bound, upper_bound) for _ in range(num_limit)]
-    evens = 0
-    odds = 0
-    for i in range(0, num_limit):
-        if i % 2 == 0:
-            evens += number[i]
-        elif i % 2 != 0:
-            odds += number[i]
+    evens = sum(n for n in number if n % 2 == 0)
+    odds = sum(n for n in number if n % 2 != 0)
+    print(odds, evens, number)
     return evens - odds
 
 
