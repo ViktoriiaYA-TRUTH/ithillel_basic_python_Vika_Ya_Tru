fede8079b6f660c0e58b4e41d7a0bbb507d3e3b2
ViktoriiaYA-TRUTH <123602119+ViktoriiaYA-TRUTH@users.noreply.github.com>
2023-03-08 06:11:57 +0200

    hw9 task 5

--- homework_9/task_5.py
+++ homework_9/task_5.py
@@ -1,21 +1,21 @@
 def group_by_surname(list_of_enrollees): # returns 4 ints
+    a_i, j_p, q_t, u_z = 0, 0, 0, 0
     for name in list_of_enrollees:
         last_name = name.split()[-1]
-        print(last_name[0])
-        a_i, j_p, q_t, u_z = 0, 0, 0, 0
-        if 'A' >= last_name[0] < 'J':
+        upper_letter = last_name[0].upper()
+        if 'A' >= upper_letter < 'J':
             a_i += 1
-        elif 'J' >= last_name[0] < 'Q':
+        elif 'J' >= upper_letter < 'Q':
             j_p += 1
-        elif 'Q' >= last_name[0] < 'U':
+        elif 'Q' >= upper_letter < 'U':
             q_t += 1
-        elif last_name[0] >= 'U':
+        elif upper_letter >= 'U':
             u_z += 1
     return a_i, j_p, q_t, u_z
 
 
 def main():
-    list_of_enrollees = ['Jay Z', 'Will Smith', 'Brad Pitt', 'Josh Bush', 'Amy Lee']
+    list_of_enrollees = ['Jay z', 'Will smith', 'Brad Pitt', 'Josh Bush', 'Amy Lee']
     print(group_by_surname(list_of_enrollees))
 
 
