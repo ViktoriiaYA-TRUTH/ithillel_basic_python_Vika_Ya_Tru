b54dea1ad5f409e2054276f955fd121453906948
ViktoriiaYA-TRUTH <123602119+ViktoriiaYA-TRUTH@users.noreply.github.com>
2023-03-09 17:55:54 +0200

    hw10 task 2

---
+++ homework_10/task_2.py
@@ -0,0 +1,32 @@
+import string
+import secrets
+
+
+def gen_password(len):
+    alphabet = string.ascii_letters + string.digits + '_'
+    while True:
+        password = ''.join(secrets.choice(alphabet) for i in range(len))
+
+
+        if (any(c.islower() for c in password)
+            and any(c.isupper() for c in password)
+            and sum(c.isdigit() for c in password)):
+            break
+
+    for c in password:
+        if c * 3 in password:
+            return False
+
+    if len < 8:
+        print('Your password must be at least 8 characters')
+        return False
+
+    return password
+
+
+def main():
+    print(gen_password(8))
+
+
+if __name__ == '__main__':
+    main()
