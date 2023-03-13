46e4d6e7d2e2404bdea991b2ca9893be82962772
ViktoriiaYA-TRUTH <123602119+ViktoriiaYA-TRUTH@users.noreply.github.com>
2023-03-13 10:31:10 +0200

    hw10 task 3 works only with first word

--- homework_10/task_3.py
+++ homework_10/task_3.py
@@ -1,26 +1,25 @@
 import random
 
 
-def permtuate(text):  # returns permuted text
+def permtuate(text):
     words = text.split(' ')
 
     for word in words:
-        if len(word) <= 3:
-            return word
         first_char = word[0]
         last_char = word[-1]
         middle = word[1:-1]
-
         shuffled_middle_chars_list = []
+
         while len(middle) >= 3:
             every_three = middle[:3]
             middle = middle[3:]
             shuffled_3 = ''.join(random.sample(every_three, len(every_three)))
+
             if shuffled_3 != every_three:
                 shuffled_middle_chars_list.append(shuffled_3)
+
         if middle:
             shuffled_middle_chars_list.append(middle)
-            print(shuffled_middle_chars_list)
 
     middle_good = ''.join(shuffled_middle_chars_list)
     shuffled_words = [first_char + middle_good + last_char for _ in words]
