2a2560ca93d8d6dc245177aff2bcb3c2c5e6c359
ViktoriiaYA-TRUTH <123602119+ViktoriiaYA-TRUTH@users.noreply.github.com>
2023-03-18 20:13:49 +0200

    hw13 changed, docstrings added

--- homework_13/task_1.py
+++ homework_13/task_1.py
@@ -1,10 +1,12 @@
 
 class Godzilla:
+    """This class defines Godzilla, it's stomach maximum and current volume"""
     def __init__(self, stomach_vol):
         self.stomach_vol = stomach_vol
         self.current_stomach_volume = 0
 
     def eat(self, human_vol):
+        """Calculates current stomach volume and prints if Godzilla is hungry or not"""
         if self.current_stomach_volume + human_vol > 0.9 * self.stomach_vol:
             print('Я з\'їв надто багато людей, більше не можу')
         else:
@@ -13,6 +15,7 @@ class Godzilla:
 
 
 def main():
+    """Makes def eat work with actual parameters"""
     a = Godzilla(50)
     a.eat(20)
     a.eat(26)
--- homework_13/task_2.py
+++ homework_13/task_2.py
@@ -2,30 +2,31 @@ import math
 
 
 class Circle:
+    """Class defines the coordinates for center of a circle and radius of a circle"""
     def __init__(self, x, y, radius):
         self.x = x
         self.y = y
         self.radius = radius
 
-    def inside_or_not(self, x_for_dot, y_for_dot):
-        d = math.sqrt((x_for_dot - self.x) ** 2 + (y_for_dot - self.y) ** 2)
-        if d <= self.radius:
-            return True
-        else:
-            return False
+    def inside_or_not(self, point):
+        """If point is inside a circle def inside_or_not returns True and if not - False"""
+        d = math.sqrt((point.x - self.x) ** 2 + (point.y - self.y) ** 2)
+        return d <= self.radius
 
 
-class Dot:
-    def __init__(self, x_for_dot, y_for_dot):
-        self.x_for_dot = x_for_dot
-        self.y_for_dot = y_for_dot
+class Point:
+    """Defines coordinates of a point"""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 def main():
+    """Defines is the point inside a circle or not with actual parameters"""
     a = Circle(4, 4, 16)
-    b = Dot(0, 2)
+    b = Point(18, 12)
     result = a.inside_or_not(b)
-    print(a.inside_or_not(15, 35))
+    print(result)
 
 
 if __name__ == '__main__':
